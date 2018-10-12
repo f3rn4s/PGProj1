1363bb8cab637f23640e2fb625186dfa5a9b576c
Fernas <fernandovgduarte@gmail.com>
2018-10-12 09:10:24 +0100

    Updated: Algorithm

--- Problem5.c
+++ Problem5.c
@@ -1,9 +1,30 @@
 //Projecto Ex5
+/*Utilize a série apresentada abaixo na realização de um programa que calcula
+ um aproximado de π, lendo para tal do standard input (teclado) o número n de 
+ termos da série, entre 20 e 100. O cálculo do valor de π é determinado ao fim 
+ das n iterações. Escreva esse valor no standard output (terminal).
+Nota: na resolução desta questão não pode usar funções da biblioteca de C*/
 
 #include <stdio.h>
 
 int main()
 {
-	
-	return 0;
+    
+    //
+    int n=0;
+    int k = 0;
+    double pi = 0.0;
+
+    for (int i = 0; i < n; i++, k++)
+    {
+        if ( k % 2 == 1)
+        {
+            pi += (double)4*(1/((2*k)-1));
+        }
+        else
+        {
+            pi += (double)4*(-1/((2*k)-1));
+        }
+    }
+    return 0;
 }
